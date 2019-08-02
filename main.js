955cd43624e01a215a786690750af7ee3a16fcf9
brampijper <brampijper@gmail.com>
2019-08-02 12:25:00 +0300

    The canvas now redraws to the correct size when the user changes the browser size

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
 let canvas = document.getElementById('canvas-grid');
 let ctx = canvas.getContext('2d');
 
-// retreive browser space and apply to the canvas
-const width = document.body.clientWidth;
-const height = document.body.clientHeight;
-canvas.width = width;
-canvas.height = height;
-
 // dimensions for one square: 100x100
 let squareSize = 100;
 
 // holds all the squares
 let squares = [];
 
-// total pixels available
-let amountOfPixels = width * height;
+window.addEventListener("resize", setupCanvasGrid)
 
-// calculating the amount of squares for the available space.
-let amountOfSquares = amountOfPixels / (squareSize * squareSize);
-// console.log('amountOfSquares ', amountOfSquares)
+function setupCanvasGrid() {
+    //reset squareholder on resize
+    squares = [];
 
-// How many squares fit in 1 row and 1 column, since width and height are often not the same value.
-let amountOfRows = height / squareSize;
-console.log('amountOfRows ', amountOfRows)
+    // retreive browser space and apply to the canvas
+    const width = document.body.clientWidth;
+    const height = document.body.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
 
-let amountOfColumns = width / squareSize;
-console.log('amountOfColumns ', amountOfColumns)
+    // total pixels available
+    let amountOfPixels = width * height;
+
+    // calculating the amount of squares for the available space.
+    let amountOfSquares = amountOfPixels / (squareSize * squareSize);
+    // console.log('amountOfSquares ', amountOfSquares)
+
+    // How many squares fit in 1 row and 1 column, since width and height are often not the same value.
+    let amountOfRows = height / squareSize;
+    console.log('amountOfRows ', amountOfRows)
+
+    let amountOfColumns = width / squareSize;
+    console.log('amountOfColumns ', amountOfColumns)
+
+    columnsLongerThanRows(amountOfRows, amountOfColumns, amountOfSquares)
+
+}
 
 // since there is a difference between rows and height it's important to know the longest one to fill the whole screen. 
-function columnsLongerThanRows() {
-    amountOfColumns > amountOfRows ? buildGrid(amountOfColumns) : buildGrid(amountOfRows);
+function columnsLongerThanRows(rows, columns, amountOfSquares) {
+    columns > rows ? buildGrid(columns, amountOfSquares) : buildGrid(rows, amountOfSquares);
 }
 
 
@@ -37,7 +47,7 @@ function columnsLongerThanRows() {
     This function places squares on the correct position in the canvas element, using the previous calculated values.
     It then pushes all squares (with their properties) in the squares array, so I can use them later on for the mousemove function.
 */
-function buildGrid(longest) {
+function buildGrid(longest, amountOfSquares) {
     for (let i = 0; i < amountOfSquares; i++) {
         squarePos = squareSize * i;
 
@@ -87,6 +97,7 @@ function opacity(opacity) {
     return opacity ? (parseFloat(opacity) + 0.01) : 0.02;
 }
 
+
 // function random(max) {
 //     return Math.floor(Math.random() * Math.floor(max))
 // }
@@ -95,12 +106,12 @@ function opacity(opacity) {
 
 /*
     - implement toolbar.
-    - adjust square size (without losing current drawing).
+    - adjust square size. (it resets the drawing)
     - screenshot -> save current drawing.
     - switch to eraser.
     - switch to different colours (on hover).
     - decrease or increase opacity strength.
     - it should also work on touch events. 
-    - redraw canvas on browser resize.
+    - Should there be a prompt when the browser wants to reset?
 */
   
