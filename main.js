ccd9877326a1fd624b522238b26e6d57dd16efd3
brampijper <brampijper@gmail.com>
2019-08-02 10:26:34 +0300

    Added comments and implemented a function that builds the grid based on the longest amount of columns or rows

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
 let canvas = document.getElementById('canvas-grid');
 let ctx = canvas.getContext('2d');
 
+// retreive browser space and apply to the canvas
 const width = document.body.clientWidth;
 const height = document.body.clientHeight;
-
 canvas.width = width;
 canvas.height = height;
 
+// dimensions for one square: 100x100
 let squareSize = 100;
+
+// holds all the squares
 let squares = [];
 
-
-// Need to know the total amount of pixels available
+// total pixels available
 let amountOfPixels = width * height;
 
-// So I can calculate what the width and height of 1 square is
+// calculating the amount of squares for the available space.
 let amountOfSquares = amountOfPixels / (squareSize * squareSize);
-console.log('amountOfSquares ', amountOfSquares)
+// console.log('amountOfSquares ', amountOfSquares)
 
-
-// Need to calculate the amount of rows
+// How many squares fit in 1 row and 1 column, since width and height are often not the same value.
 let amountOfRows = height / squareSize;
 console.log('amountOfRows ', amountOfRows)
 
-function buildGrid() {
+let amountOfColumns = width / squareSize;
+console.log('amountOfColumns ', amountOfColumns)
+
+// since there is a difference between rows and height it's important to know the longest one to fill the whole screen. 
+function columnsLongerThanRows() {
+    amountOfColumns > amountOfRows ? buildGrid(amountOfColumns) : buildGrid(amountOfRows);
+}
+
+
+/* 
+    This function places squares on the correct position in the canvas element, using the previous calculated values.
+    It then pushes all squares (with their properties) in the squares array, so I can use them later on for the mousemove function.
+*/
+function buildGrid(longest) {
     for (let i = 0; i < amountOfSquares; i++) {
         squarePos = squareSize * i;
 
-        for (let x = 0; x < amountOfRows; x++) {
+        for (let x = 0; x < longest; x++) {
             ctx.fillRect(squareSize * i , x * squareSize, squareSize, squareSize)
             ctx.fillStyle = 'rgba(0,0,0,0)'
             squares.push({
@@ -42,7 +56,7 @@ function buildGrid() {
 }
 
 canvas.addEventListener('mousemove', (e) => {
-    console.log(e)
+    // console.log(e)
     const mousePos = {
         x: e.clientX - canvas.offsetLeft,
         y: e.clientY - canvas.offsetTop
@@ -51,8 +65,8 @@ canvas.addEventListener('mousemove', (e) => {
     let gridX = Math.floor(mousePos.x / squareSize);
     let gridY = Math.floor(mousePos.y / squareSize);
 
-    console.log('gridX, ', gridX)
-    console.log('gridY, ', gridY)
+    // console.log('gridX, ', gridX)
+    // console.log('gridY, ', gridY)
 
     let testX = gridY * squareSize; 
     let testY = gridX * squareSize;
